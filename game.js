711b6ad82d7a6427dfc67d8bf67904182e0a006d
Rob Ede <robjtede@gmail.com>
2015-11-14 15:20:58 +0000

    Added stylesheet and test sprite movement.

--- game.js
+++ game.js
@@ -1,5 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+var s;
+
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "our-game", {
+	preload: preload,
+	create: create,
+	update: update,
+	render: render
+});
+
+function preload () {
+	
+	game.load.image("character", "./assets/man.png");
+	
+}
+
+
+function create () {
+	
+	s = game.add.sprite(game.world.centerX, game.world.centerY, "man");
+	
+}
+
+function update () {
+	
+	var speed = 4;
+	
+	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+		s.x -= speed;
+		
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+		s.x += speed;
+		
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+		s.y -= speed;
+		
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+		s.y += speed;
+		
+	}
+	
+}
+
+function render () {
 	
 	
 	
+}
+
 });
