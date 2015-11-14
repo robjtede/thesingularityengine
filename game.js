9d23d421726f38bddc95f7c5d2c6b5049885aa54
Rob Ede <robjtede@gmail.com>
2015-11-14 21:46:17 +0000

    Tilemap and boundary detection

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-var s; // sprite reference placeholder
-var speed = 4;
+var player; // player sprite
+var world; // world collision group
+var map; // tilemap
+
+var cursors; // controls
+
+var speed = 300; // character movement speed
 
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "our-game", {
 	preload: preload,
@@ -12,41 +17,78 @@ var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "
 
 function preload () {
 	
+	game.load.image("world", "assets/world.jpg");
 	game.load.image("man", "assets/man.png");
+	game.load.image("box", "assets/box.png");
+	game.load.tilemap("map", "assets/tilemap.csv", null, Phaser.Tilemap.CSV);
 	
-}
+}// preload()
 
 
 function create () {
 	
-	s = game.add.sprite(game.world.centerX, game.world.centerY, "man");
+	map = game.add.tilemap("map", 200, 200);
+	map.addTilesetImage("world");
 	
-}
+	world = map.createLayer(0);
+	world.resizeWorld();
+	map.setCollisionBetween(0, 1);
+	world.debug = true;
+	
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+	player = game.add.sprite(game.world.centerX, game.world.centerY, "man");
+	game.physics.enable(player, Phaser.Physics.ARCADE);
+	player.body.setSize(50, 80, 0, 0);
+	
+	game.camera.follow(player);
+	
+	
+	
+	
+}// create()
 
 function update () {
 	
+	game.physics.arcade.collide(player, world);
+	
+	player.body.velocity.set(0);
+	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-		s.x -= speed;
+		player.body.velocity.x = -speed;
 		
 	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-		s.x += speed;
+		player.body.velocity.x = speed;
+		
+	} else {
+		player.body.velocity.x = 0;
 		
 	}
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-		s.y -= speed;
+		player.body.velocity.y = -speed;
 		
 	} else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-		s.y += speed;
+		player.body.velocity.y = speed;
+		
+	} else {
+		player.body.velocity.y = 0;
 		
 	}
 	
-}
+	
+}// update()
+
+function collision (p, obj) {
+	
+	console.log(p.body.touching);
+	
+}// collision()
 
 function render () {
 	
-	game.debug.spriteInfo(s, 20, 32);
+	game.debug.spriteInfo(player, 20, 32);
 	
-}
+}// render()
 
 });
