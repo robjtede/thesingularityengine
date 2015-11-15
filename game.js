e48111c399e3244b82ec87c92d6bab4a457aa2b9
Rob Ede <robjtede@gmail.com>
2015-11-15 02:29:05 +0000

    Full sprite animations added

--- game.js
+++ game.js
@@ -19,7 +19,7 @@ var fader = new Box(game);
 function preload () {
 	
 	game.load.image("world", "assets/world.jpg");
-	game.load.image("man", "assets/man.png");
+	game.load.spritesheet("man", "assets/guy.png", 50, 80);
 	game.load.image("box", "assets/box.png");
 	game.load.tilemap("map", "assets/tilemap.csv", null, Phaser.Tilemap.CSV);
 	game.load.spritesheet("boxFade", "assets/fade.png", 1000, 200, 3);
@@ -41,22 +41,24 @@ function create () {
 	
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	
-	player = game.add.sprite(game.world.centerX, game.world.centerY, "man");
+	player = game.add.sprite(game.world.centerX, game.world.centerY, "man", 0);
+	player.animations.add("backwards", [9, 11, 10, 11], 5, true);
+	player.animations.add("forwards", [1, 0, 2, 0], 5, true);
+	player.animations.add("left", [4, 3, 5, 3], 5, true);
+	player.animations.add("right", [6, 8, 7, 8], 5, true);
+	
 	game.physics.enable(player, Phaser.Physics.ARCADE);
 	player.body.setSize(50, 80, 0, 0);
 	
 	game.camera.follow(player);
 	
 	
-	
-	
-	s = game.add.sprite(game.world.centerX, game.world.centerY, "man");
 	fader.init();
 } // create()
 
 function update () {
 	
-	fader.moveTo(player.x,player.y);
+	fader.moveTo(player.x, player.y);
 	fader.update();
 
 	game.physics.arcade.collide(player, world);
@@ -65,27 +67,27 @@ function update () {
 	
 	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
 		player.body.velocity.x = -speed;
+		player.play("left");
 		
 	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
 		player.body.velocity.x = speed;
+		player.play("right");
 		
-	} else {
-		player.body.velocity.x = 0;
-		
-	}
-	
-	if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
 		player.body.velocity.y = -speed;
+		player.play("backwards");
 		
 	} else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
 		player.body.velocity.y = speed;
+		player.play("forwards");
 		
 	} else {
 		player.body.velocity.y = 0;
+		player.animations.stop();
 		
 	}
 
-	if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
+	if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
 		fader.setRatio(0);
 	}
 	
