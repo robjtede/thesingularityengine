f0b8b028b47a2e3fa4698b07a3873c8fd7874664
Rob Ede <robjtede@gmail.com>
2015-11-15 09:52:02 +0000

    Initial tilemap (removed collisions temporarily)

--- game.js
+++ game.js
@@ -5,7 +5,9 @@ var map; // tilemap
 
 var cursors; // controls
 
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "our-game", {
+var speed = 200; // character movement speed
+
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "endoftheworld", {
 	preload: preload,
 	create: create,
 	update: update,
@@ -22,7 +24,7 @@ var badBubbles = [];
 
 function preload () {
 	
-	game.load.image("world", "assets/map.png");
+	game.load.image("world", "assets/map2.png");
 	game.load.tilemap("map", "assets/tilemap.csv", null, Phaser.Tilemap.CSV);
 	
 	game.load.spritesheet("man", "assets/guy.png", 50, 80);
@@ -31,19 +33,19 @@ function preload () {
 	game.load.spritesheet("goodBubble", "assets/goodBubble.png", 28, 25, 2);
 	game.load.spritesheet("badBubble", "assets/badBubble.png", 28, 25, 2);
 
-	for(var i = 0; i<100; i++)
-	{
+	for (var i = 0; i<100; i++) {
 		goodBubbles[i] = new Bubble(game, Math.random()*6000 + 600, Math.random() * 2000 + 400);
-	       	badBubbles[i] = new Bubble(game, Math.random()*6000+600, Math.random() * 2000 + 400);
+		badBubbles[i] = new Bubble(game, Math.random()*6000 + 600, Math.random() * 2000 + 400);
 	}
-}
+	
+}// preload()
 
 
 function create () {
 	
 	game.stage.backgroundColor = 0x999999;
 	
-	map = game.add.tilemap("map", 80, 80);
+	map = game.add.tilemap("map", 100, 100);
 	map.addTilesetImage("world");
 
 	music.create(this);
@@ -51,43 +53,52 @@ function create () {
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	world = map.createLayer(0);
 	world.resizeWorld();
+	world.debug = true;
+	
+	// collisions
 	map.setCollisionBetween(0, 1);
-
+	
 	player.init();
 	//bubble.init("bad", player);
-	for(var i = 0; i<100; i++)
-	{
+	for (var i = 0; i<100; i++) {
 		goodBubbles[i].init("good", player);
 		badBubbles[i].init("bad", player);
 	}
+	
+	player = game.add.sprite(game.world.centerX, game.world.centerY, "man", 0);
+	player.animations.add("backwards", [9, 11, 10, 11], 5, true);
+	player.animations.add("forwards", [1, 0, 2, 0], 5, true);
+	player.animations.add("left", [4, 3, 5, 3], 5, true);
+	player.animations.add("right", [6, 8, 7, 8], 5, true);
+	
+	game.physics.enable(player, Phaser.Physics.ARCADE);
+	player.body.setSize(50, 80, 0, 0);
+	
+	game.camera.follow(player);
+	
 	fader.init();
 	
-} // create()
+}// create()
 
 function update () {
 	
 	fader.moveTo(player.getX(), player.getY());
 	fader.update();	
 	//bubble.update();
-	for (var i = 0; i<100; i++)
-	{
+	for (var i = 0; i<100; i++) {
 		goodBubbles[i].update();
 		badBubbles[i].update();
 	}
+	
 	game.physics.arcade.collide(player.getSprite(), world);
 	player.update();
-	if (player.getHealth() == 0)
-	{
+	
+	if (player.getHealth() == 0) {
 		window.location.href = "end.html";
 	}
+	
 }// update()
 
-/*function collision (p, obj) {
-	
-	console.log(p.touching);
-	
-}// collision()
-*/
 function render () {
 	
 	game.debug.spriteInfo(player.getSprite(), 20, 32);
